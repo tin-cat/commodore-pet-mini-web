b1237a676b70dc8b2fa9cfc321eaa2d934077ce0
Lorenzo <lorenzo@tin.cat>
2019-01-22 09:47:46 +0000

    Forcing logo change in browser cache via version parameter

--- config/UiComponents/UiComponentPanel.config.php
+++ config/UiComponents/UiComponentPanel.config.php
@@ -13,7 +13,7 @@ $UiComponentPanelConfig = [
     "theme" => false, // The default theme. Set it to false to not apply any specific theme
     "style" => "",
     "logo" => [ // Sets up the logo
-        "fullImageUrl" => "/res/img/commodorePetMiniLogoWhite.svg", // The image to show as logo when the panel is working in a big screen
+        "fullImageUrl" => "/res/img/commodorePetMiniLogoWhite.svg?v=2", // The image to show as logo when the panel is working in a big screen
         "smallImageUrl" => "/res/img/commodorePetMiniIconWhite.svg", // The image to show as logo when the panel is working in a small screen
         "linkRequest" => "homePage" // If set, the logo will link to this request
     ],
