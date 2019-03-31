4b6f960c720338fb84802016ad722d38cc73e466
Loren <lorenzo@tin.cat>
2019-03-31 10:36:02 -0500

    Link to download frmo the repository now links directly to the zip file, not to GitHub home page

--- config/App.config.php
+++ config/App.config.php
@@ -13,6 +13,7 @@ namespace CherrycakeApp;
 const DIR_RES = "res"; // The directory where resources like css, javascript, images and fonts are stored
 
 define("REPOSITORY_URL", "https://github.com/tin-cat/commodore-pet-mini");
+define("REPOSITORY_DIRECT_DOWNLOAD_URL", "https://github.com/tin-cat/commodore-pet-mini/archive/master.zip");
 define("REPOSITORY_URL_WEB", "https://github.com/tin-cat/commodore-pet-mini-web");
 
 define("TWITTER_URL", "https://twitter.com/lorenzoherrera");
