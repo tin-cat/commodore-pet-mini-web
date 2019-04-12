e7510aa912d4eecc6329c6e8b91fc577fae7c855
Loren <lorenzo@tin.cat>
2019-04-12 04:44:52 -0500

    Added Build SN 0001 with photos of the very same Chuck Peddle holding it thanks to Vince Weaver!

--- config/UserBuilds.config.php
+++ config/UserBuilds.config.php
@@ -17,7 +17,7 @@ $UserBuildsConfig = [
             "name" => "Vince Weaver",
             "url" => "https://twitter.com/deater78",
             "date" => mktime(0, 0, 0, 4, 5, 2019),
-            "text" => "Thanks to Vince, this little buddy has been showcased in an exhibition held at the <a href=\"https://umaine.edu/\" target=\"external\">University of Maine</a>, where <a href=\"https://www.commodore.ca/commodore-history/the-legendary-chuck-peddle-inventor-of-the-personal-computer/\" target=\"external\">Chuck Peddle</a> itself, the designer of the authentic Commodore PET and the 6502 microprocessor, a visionary computer pioneer who shaped the modern era of computing. You can see him on the first photo holding this Commodore PET Mini. Thanks Mr.Peddle, we're humbled!"
+            "text" => "Thanks to Vince, this little buddy has been showcased in an exhibition held on april this year at the <a href=\"https://umaine.edu/\" target=\"external\">University of Maine</a>, where <a href=\"https://www.commodore.ca/commodore-history/the-legendary-chuck-peddle-inventor-of-the-personal-computer/\" target=\"external\">Chuck Peddle</a> itself, the designer of the authentic Commodore PET and the 6502 microprocessor, and a visionary computer pioneer who shaped the modern era of computing gave a speech and found time to take a photo holding this Commodore PET Mini. Thanks Mr.Peddle for giving us great computers!"
         ],
         [
             "serialNumber" => 0,
