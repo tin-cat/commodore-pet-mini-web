d96621fff1c294be83b10f6cd29cf8cd40b257f3
Loren <lorenzo@tin.cat>
2020-02-29 12:24:22 +0100

    Removed embedded videos form home page because YouTube adds a huge javascript overhead (many Mbs) and also Google Ad tracking cookies

--- load.php
+++ load.php
@@ -2,4 +2,4 @@
 
     // Include the cherrycake loader script, set this to the proper path to your installation of the Cherrycake engine
     // Since the Cherrycake engine is normally installed via composer, this should normally be set to "vendor/tin-cat/cherrycake/load.php"
-    require "/engine/load.php";
+    require "vendor/tin-cat/cherrycake-engine/load.php";
