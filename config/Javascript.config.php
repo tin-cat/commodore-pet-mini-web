958fb3cd27499a78d605437f9ba53e2e0bffd4ae
Loren <lorenzo@tin.cat>
2020-05-24 19:08:59 +0200

    WIP

--- config/Javascript.config.php
+++ config/Javascript.config.php
@@ -14,7 +14,7 @@ global $e;
 
 $JavascriptConfig = [
 	"lastModifiedTimestamp" => mktime(2, 0, 0, 22, 1, 2019), // The global version
-	"isMinify" => !$e->isDevel(),
+	"isMinify" => false,
 	"sets" => [ // An array of Javascript sets with its files that should be always configured, will be added to the group "main"
 		"main" => [
 			"directory" => \CherrycakeApp\DIR_RES."/javascript/main"
