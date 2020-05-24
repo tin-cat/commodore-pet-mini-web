c03482e788c15d067ac70f6014d37484e9d1fce0
Loren <lorenzo@tin.cat>
2020-05-24 18:56:59 +0200

    WIP

--- load.php
+++ load.php
@@ -3,4 +3,4 @@
     // Include the cherrycake loader script, set this to the proper path to your installation of the Cherrycake engine
     // Since the Cherrycake engine is normally installed via composer, this should normally be set to "vendor/tin-cat/cherrycake/load.php"
 
-	require in_array($_SERVER["HTTP_HOST"], ["commodorepetmini.com.buzz", "localhost"]) ? "/engine/load.php" : "../vendor/tin-cat/cherrycake-engine/load.php"
+	require in_array($_SERVER["HTTP_HOST"], ["commodorepetmini.com.buzz", "localhost"]) ? "/engine/load.php" : "../vendor/tin-cat/cherrycake-engine/load.php";
