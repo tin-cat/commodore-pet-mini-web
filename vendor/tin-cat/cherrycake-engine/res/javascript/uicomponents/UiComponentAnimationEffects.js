dac3f8e99b222904dcbe2c4c1785373fd008e1d2
Loren <loren@tin.cat>
2021-02-13 21:26:48 +0100

    WIP

--- vendor/tin-cat/cherrycake-engine/res/javascript/uicomponents/UiComponentAnimationEffects.js
+++
@@ -1,8 +0,0 @@
-function animationEffectShake(element) {
-	var times = 3;
-	var distance = 5;
-	var interval = 70;
-	for (var i=0; i<(times+1); i++)
-		$(element).animate({left: ((i%2==0 ? distance : distance*-1))}, interval);
-	$(element).animate({left: 0},interval);
-}
